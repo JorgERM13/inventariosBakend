7636fe72220fc2283ad36dc48062dd77998c0065
Jorge Rojas Mejia <jerm0012@gmail.com>
2025-11-26 21:49:12 -0400

    Crud completo usuarios

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -2,9 +2,85 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsuarioController extends Controller
 {
-    //
+    public function funListar(){
+
+        try {
+            $usuarios = User::get();
+            return response()->json($usuarios, 200);
+
+        } catch (\Exception $e) {
+            return response()->json(["mensaje" => $e->getMessage()], 500);
+        }
+
+    }
+    public function funGuardar(Request $request){
+
+        $request->validate([
+            'name' => 'required',
+            'email' => 'email|required|unique:users',
+            'password' => 'required|min:6|max:30'
+        ]);
+
+         try{
+           $usuario= new User();
+
+          $usuario->name = $request->name;
+          $usuario->email = $request->email;
+          $usuario->password = $request->password;
+          $usuario->save();
+
+          return response()->json(["mensaje" => "Usuario Registrado"], 201);
+
+        }catch (\Exception $e) {
+            return response()->json(["mensaje" => $e->getMessage()], 500);
+        }
+    }
+    public function funMostrar($id){
+
+        try{
+            $usuario = User::find($id);
+
+            return response()->json($usuario, 200);
+
+        }catch (\Exception $e) {
+            return response()->json(["mensaje" => $e->getMessage()], 500);
+        }
+
+    }
+    public function funModificar(Request $request, $id){
+
+
+        try{
+
+          $usuario= User::find($id);
+          $usuario->name = $request->name;
+          $usuario->email = $request->email;
+          $usuario->password = $request->password;
+          $usuario->update();
+
+           return response()->json(["mensaje" => "Usuario Modificado"], 201);
+
+        }catch (\Exception $e) {
+            return response()->json(["mensaje" => $e->getMessage()], 500);
+        }
+
+    }
+    public function funEliminar($id){
+
+        try{
+
+        $usuario= User::find($id);
+        $usuario->delete();
+        return response()->json(["mensaje" => "Usuario Eliminado"], 200);
+
+        }catch (\Exception $e) {
+            return response()->json(["mensaje" => $e->getMessage()], 500);
+        }
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsuarioController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+//CRUD usuarios
+Route::get("/usuario", [UsuarioController::class, "funListar"]);
+Route::post("/usuario", [UsuarioController::class, "funGuardar"]);
+Route::get("/usuario/{id}", [UsuarioController::class, "funMostrar"]);
+Route::put("/usuario/{id}", [UsuarioController::class, "funModificar"]);
+Route::delete("/usuario/{id}", [UsuarioController::class, "funEliminar"]);
